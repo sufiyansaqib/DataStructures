5eca274815ae96f6d311175bae3d45cdb3a988b2
sufiyansaqib <34838688+sufiyansaqib@users.noreply.github.com>
2020-01-15 15:23:19 +0530

    Update Take_Input - O(n^2).cpp

--- Linked_List/Take_Input - O(n^2).cpp
+++ Linked_List/Take_Input - O(n^2).cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 using namespace std;
 
@@ -21,7 +13,8 @@ class Node{
 };
 
 Node* insert(){
-    cout<<"ENTER NUMBERS , ENTER -1 TO STOP :";
+    // Insert till "-1" is encountered
+    cout<<"ENTER NUMBERS , ENTER -1 TO STOP :"; 
     int data;
     cin>>data;
     
