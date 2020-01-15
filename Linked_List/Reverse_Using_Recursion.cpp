aa6d25f8d5a4e9793d0006ab332537fe42eb6ecf
sufiyansaqib <34838688+sufiyansaqib@users.noreply.github.com>
2020-01-15 20:14:29 +0530

    Create Reverse_Using_Recursion.cpp

---
+++ Linked_List/Reverse_Using_Recursion.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+            int data;
+            Node *next;
+            
+            Node(int data){
+                this->data = data;
+                this->next = NULL;
+            }
+};
+
+void print(Node *head){
+    Node *temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+Node* reverse(Node *head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    
+    Node *final_head = reverse(head->next);
+    Node *temp = final_head;
+    
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = head;
+    head->next = NULL;
+    return final_head;
+    
+}
+
+int main()
+{
+    Node *n1 = new Node(1);
+    Node *n2 = new Node(2);
+    Node *n3 = new Node(3);
+    Node *n4 = new Node(4);
+    Node *n5 = new Node(5);
+    
+    Node *head = n1;
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n5;
+    print(head);
+    
+    head = reverse(head);
+    print(head);
+    return 0;
+}
